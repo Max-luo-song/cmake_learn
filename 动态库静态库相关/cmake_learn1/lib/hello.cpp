24b7efc1b060fe3174f8c9652fe39f4d193e5e58
Max-luo-song <58042007+Max-luo-song@users.noreply.github.com>
2022-06-06 20:31:36 +0800

    Add files via upload

---
+++ 动态库静态库相关/cmake_learn1/lib/hello.cpp
@@ -0,0 +1,6 @@
+#include"hello.h"
+#include<iostream>
+
+void HelloFunc() {
+	std::cout << "hello world!" << std::endl;
+}
---
+++ 动态库静态库相关/cmake_learn1/lib/hello.h
@@ -0,0 +1,5 @@
+#ifndef HELLO_H
+#define HELLO_H
+void HelloFunc();
+
+#endif
